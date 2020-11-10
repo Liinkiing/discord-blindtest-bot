626867f776fb2d366418a539d48b4066f82d9327
Omar Jbara <omar.jbara2@gmail.com>
2020-11-11 00:10:59 +0100

    - Prevent duplicates by title

--- scripts/spotify/index.ts
+++ scripts/spotify/index.ts
@@ -10,37 +10,51 @@ const removeParens = true
   // const client = new Spotify({
   //   accessToken: process.env.SPOTIFY_ACCESS_TOKEN,
   // })
+  let titles = (await AirtableApiClient.songs().select().all()).map(
+    record => record.get('Title') as string
+  )
+  let i = 0
   for (const track of tracks) {
     // const artistId = track.artistLink!.split('/').pop()!
     // const artist = await client.getArtist(artistId)
     let title = track.title
     if (removeAfterDash) {
       title = title.split('-')[0]
+      titles = titles.map(t => t.split('-')[0])
     }
     if (removeParens) {
       title = title.replace(/\([^()]*\)/g, '')
+      titles = titles.map(t => t.replace(/\([^()]*\)/g, ''))
     }
     if (removeThemeWord) {
-      title = title.replace(/theme/gi, '')
-      title = title.replace(/main title/gi, '')
+      title = title.replace(/(theme|main title)/gi, '')
+      titles = titles.map(t => t.replace(/(theme|main title)/gi, ''))
     }
     title = title.trim()
-    console.log('Adding ' + title + '\n')
-    await AirtableApiClient.songs()
-      .create(
-        {
-          'Preview URL': track.preview,
-          Title: title,
-          Artists: track.artist.split(',').map(s => s.trim()),
-          Genres: [],
-          Album: track.album,
-          Picture: track.picture,
-          Link: track.trackLink,
-        },
-        { typecast: true }
-      )
-      .catch(er => {
-        console.log(er)
-      })
+    titles = titles.map(t => t.trim())
+    if (titles.includes(title)) {
+      console.log(`Skipping ${title} because it already exists...`)
+      i = i + 1
+    } else {
+      console.log('Adding ' + title + '\n')
+      await AirtableApiClient.songs()
+        .create(
+          {
+            'Preview URL': track.preview,
+            Title: title,
+            Artists: track.artist.split(',').map(s => s.trim()),
+            Genres: ['music'],
+            Album: track.album,
+            Picture: track.picture,
+            Link: track.trackLink,
+          },
+          { typecast: true }
+        )
+        .catch(er => {
+          console.log(er)
+        })
+      i = i + 1
+    }
   }
+  console.log(`Successfully handled ${i} songs`)
 })()
